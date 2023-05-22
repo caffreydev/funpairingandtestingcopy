4b97ff79679d70c5e7f0dfffaf2b39293e64db6a
zenabhaider <zenabhaider05@gmail.com>
2023-05-22 14:55:10 +0100

    done up challenge 3

--- katas/alternate-case.js
+++ katas/alternate-case.js
@@ -1,3 +1,19 @@
-function alternateCase() {}
+function alternateCase(string) {
+    const capitalised = string.toUpperCase();
+    const noSpaces = capitalised.split(" ").join("");
+    const array = noSpaces.split('');
+
+    for (let i = 1; i < array.length; i+=2) {
+       array[i] = array[i].toLowerCase();
+    
+    }
+    
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === " ") {
+            array.splice(i , 0, " ");
+        }
+    }
+    return array.join("");
+}
 
 module.exports = alternateCase;
--- katas/count-veg.js
+++ katas/count-veg.js
@@ -1,3 +1,18 @@
-function countVeg() {}
+function countVeg(array, string) {
+    
+
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        let obj = array[i];
+        if (obj.type === string) {
+            sum += obj.quantity;
+        
+            }
+
+        
+    }
+    return sum;
+
+}
 
 module.exports = countVeg;
